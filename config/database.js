95598b61a45796e17546b75ecffeb7ce92aa7f4d
GreatEki <ekeneidiagbor@gmail.com>
2020-07-16 01:12:56 -1100

    tools in portfolio section ui change

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const DBConnnect = async () => {
 	try {
-		const conn = await mongoose.connect(process.env.mongoURI, {
+		const conn = await mongoose.connect(process.env.MONGO_URI, {
 			useNewUrlParser: true,
 			useCreateIndex: true,
 			useUnifiedTopology: true,
