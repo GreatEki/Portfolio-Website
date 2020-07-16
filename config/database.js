f7f891858368280bbfa698d0ff536e5d200fe4d9
GreatEki <ekeneidiagbor@gmail.com>
2020-07-15 23:54:39 -1100

    db file added

---
+++ config/database.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const DBConnnect = async () => {
+	try {
+		const conn = await mongoose.connect(process.env.mongoURI, {
+			useNewUrlParser: true,
+			useCreateIndex: true,
+			useUnifiedTopology: true,
+		});
+
+		console.log(`Database Connected: ${conn.connection.host}`);
+	} catch (err) {
+		console.log(`Database connection Error: ${err.message}`);
+	}
+};
+
+module.exports = DBConnnect;
